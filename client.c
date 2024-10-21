3cf54fc4b580c4a05bddd0d2cbd0785ecf6d4035
Hailin <h.zeng@ufl.edu>
2024-10-21 14:07:06 -0400

    Fix md5 func

--- client.c
+++ client.c
@@ -1,26 +1,27 @@
 /* Included libraries */
-#include <stdio.h>			/* For printf() and fprintf() */
-#include <sys/socket.h>		/* For socket(), connect(), send(), and recv() */
-#include <arpa/inet.h>		/* For sockaddr_in and inet_addr() */
-#include <stdlib.h>			/* Supports all sorts of functionality */
-#include <unistd.h>			/* For close() */
-#include <string.h>			/* Support any string ops */
+#include <stdio.h>		/* For printf() and fprintf() */
+#include <sys/socket.h> /* For socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>	/* For sockaddr_in and inet_addr() */
+#include <stdlib.h>		/* Supports all sorts of functionality */
+#include <unistd.h>		/* For close() */
+#include <string.h>		/* Support any string ops */
 
-#include <dirent.h>			/* Support any file ops */
-#include <openssl/evp.h>	/* Support md5 */
+#include <dirent.h>		 /* Support any file ops */
+#include <openssl/evp.h> /* Support md5 */
 
 /* Constants */
 #define PORT 8888
-#define BUFFER_SIZE 1024		/* The send buffer size */
+#define BUFFER_SIZE 1024 /* The send buffer size */
 
 /* Globals */
-typedef struct {
-	int num_files;			// Number of files
-	char **file_names;		// Array of file names
-	unsigned char **md5s;		// Array of md5s
+typedef struct
+{
+	int num_files;		  // Number of files
+	char **file_names;	  // Array of file names
+	unsigned char **md5s; // Array of md5s
 } file_list_t;
 
-file_list_t* file_list;
+file_list_t *file_list;
 char PATH[100] = "./music_client";
 
 /* Functions */
@@ -31,14 +32,17 @@ void pull_files(int clientSock);
 file_list_t *receive_file_list(int clientSock);
 void create_file_list(const char *dir_path);
 void free_file_list(file_list_t *file_list_ptr);
+unsigned char *compute_md5(char *file_name);
 
 /* The main function */
-int main() {
-	int clientSock;					/* socket descriptor */
-	struct sockaddr_in serv_addr;   /* The server address */
+int main()
+{
+	int clientSock;				  /* socket descriptor */
+	struct sockaddr_in serv_addr; /* The server address */
 
 	/* Create a new TCP socket*/
-	if ((clientSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+	if ((clientSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
 		printf("socket() failed\n");
 		return 1;
 	}
@@ -47,46 +51,50 @@ int main() {
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	serv_addr.sin_port = htons(PORT); 
+	serv_addr.sin_port = htons(PORT);
 
 	/* Establish connecction to the server */
-	if (connect(clientSock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-		printf("Connection failed\n"); 
+	if (connect(clientSock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("Connection failed\n");
 		return 1;
 	}
 	printf("Connected to the server.\n");
 	create_file_list(PATH);
 
 	showMenu();
-	while (1) {
+	while (1)
+	{
 		printf("\nEnter your choice: ");
 		int choice;
 		scanf("%d", &choice);
 
-		switch (choice) {
-			case 1:
-				list_files(clientSock);
-				break;
-			case 2:
-				diff_files(clientSock);
-				break;
-			case 3:
-				pull_files(clientSock);
-				break;
-			case 4:
-				send(clientSock, "LEAVE", 5, 0);
-				free_file_list(file_list);
-				close(clientSock); 
-				return 0;
-			default:
-				printf("Invalid choice. Try again.\n");
+		switch (choice)
+		{
+		case 1:
+			list_files(clientSock);
+			break;
+		case 2:
+			diff_files(clientSock);
+			break;
+		case 3:
+			pull_files(clientSock);
+			break;
+		case 4:
+			send(clientSock, "LEAVE", 5, 0);
+			free_file_list(file_list);
+			close(clientSock);
+			return 0;
+		default:
+			printf("Invalid choice. Try again.\n");
 		}
 	}
-	
+
 	return 0;
 }
 
-void showMenu() {
+void showMenu()
+{
 	printf("--- UFmyMusic Menu ---\n");
 	printf("1. List Files\n");
 	printf("2. Diff Files\n");
@@ -95,13 +103,15 @@ void showMenu() {
 }
 
 // 1. List Files: Ask the server to return a list of files it currently has
-void list_files(int clientSock) {
+void list_files(int clientSock)
+{
 	send(clientSock, "LIST", 4, 0);
-	
-	file_list_t* server_file_list = receive_file_list(clientSock);
-	
+
+	file_list_t *server_file_list = receive_file_list(clientSock);
+
 	printf("\nA List of Files that Server has:\n");
-	for (int i = 0; i < server_file_list->num_files; i++) {
+	for (int i = 0; i < server_file_list->num_files; i++)
+	{
 		printf("%s\n", server_file_list->file_names[i]);
 	}
 
@@ -109,60 +119,73 @@ void list_files(int clientSock) {
 }
 
 // 2. Diff (based on 1)): the client should show a "diff" of the files it has in comparison to the server);
-void diff_files(int clientSock) {
+void diff_files(int clientSock)
+{
 	send(clientSock, "DIFF", 4, 0);
-	
-	file_list_t* server_file_list = receive_file_list(clientSock);
-	
+
+	file_list_t *server_file_list = receive_file_list(clientSock);
+
 	// Compare remote with local
 	int flag = 0;
-	for (int i = 0; i < server_file_list->num_files; i++) {
+	for (int i = 0; i < server_file_list->num_files; i++)
+	{
 		int found = 0;
-		for (int j = 0; j < file_list->num_files; j++) {
-			if (strncmp((const char*)(server_file_list->md5s[i]), (const char*)(file_list->md5s[j]), 16) == 0) {
+		for (int j = 0; j < file_list->num_files; j++)
+		{
+			if (strncmp((const char *)(server_file_list->md5s[i]), (const char *)(file_list->md5s[j]), 16) == 0)
+			{
 				found = 1;
 				break;
 			}
 		}
-		if(!found) { // If local file not found
-			if(!flag) {
+		if (!found)
+		{ // If local file not found
+			if (!flag)
+			{
 				flag = 1;
 				printf("\nMissing Files Compared to Server:\n");
 			}
 			printf("%s\n", server_file_list->file_names[i]);
 		}
 	}
-	if(!flag) {
+	if (!flag)
+	{
 		printf("\nYou are up-to-date!\n");
 	}
-	
+
 	free_file_list(server_file_list);
 }
 
 // 3. Pull (request all files identified in 2): from the server and store them locally)
-void pull_files(int clientSock) {
+void pull_files(int clientSock)
+{
 	send(clientSock, "PULL", 4, 0);
-	
-	file_list_t* server_file_list = receive_file_list(clientSock);
-	
+
+	file_list_t *server_file_list = receive_file_list(clientSock);
+
 	// Compare remote with local
 	int flag = 0;
-	for (int i = 0; i < server_file_list->num_files; i++) {
+	for (int i = 0; i < server_file_list->num_files; i++)
+	{
 		int found = 0;
-		for (int j = 0; j < file_list->num_files; j++) {
-			if (strncmp((const char*)(server_file_list->md5s[i]), (const char*)(file_list->md5s[j]), 16) == 0) {
+		for (int j = 0; j < file_list->num_files; j++)
+		{
+			if (strncmp((const char *)(server_file_list->md5s[i]), (const char *)(file_list->md5s[j]), 16) == 0)
+			{
 				found = 1;
 				break;
 			}
 		}
-		if(!found) { // If local file not found
-			if(!flag) {
+		if (!found)
+		{ // If local file not found
+			if (!flag)
+			{
 				flag = 1;
 				printf("\nFetching Missing Files:\n");
 			}
 			printf("%s\n", server_file_list->file_names[i]);
 			send(clientSock, &i, sizeof(i), 0);
-			
+
 			// Receive file size
 			long file_size;
 			recv(clientSock, &file_size, sizeof(file_size), 0);
@@ -171,13 +194,14 @@ void pull_files(int clientSock) {
 			FILE *file;
 			size_t bytes_received;
 			long total_received = 0;
-			
+
 			// Open the file in write mode
 			snprintf(buffer, sizeof(buffer), "%s/%s", PATH, server_file_list->file_names[i]);
 			file = fopen(buffer, "wb");
 
 			// Receive the file in chunks
-			while (total_received < file_size && (bytes_received = recv(clientSock, buffer, sizeof(buffer), 0)) > 0) {
+			while (total_received < file_size && (bytes_received = recv(clientSock, buffer, sizeof(buffer), 0)) > 0)
+			{
 				fwrite(buffer, 1, bytes_received, file);
 				total_received += bytes_received;
 			}
@@ -185,35 +209,39 @@ void pull_files(int clientSock) {
 			fclose(file);
 		}
 	}
-	if(!flag) {
+	if (!flag)
+	{
 		printf("\nYou are up-to-date!\n");
 	}
-	else {
+	else
+	{
 		printf("\nDone fetching!\n");
 	}
 	int i = -1;
 	send(clientSock, &i, sizeof(i), 0);
-	
+
 	free_file_list(server_file_list);
 	free_file_list(file_list);
 	create_file_list("./music_client");
 }
 
 // Receive the file list from the server
-file_list_t *receive_file_list(int clientSock) {
+file_list_t *receive_file_list(int clientSock)
+{
 	file_list_t *server_file_list = malloc(sizeof(file_list_t));
-	
+
 	// Receive the number of files
 	int num;
 	recv(clientSock, &num, sizeof(num), 0);
 	server_file_list->num_files = num;
-	
+
 	// Allocate memory for the file names and md5s
 	server_file_list->file_names = malloc(num * sizeof(char *));
 	server_file_list->md5s = malloc(num * sizeof(unsigned char *));
-	
+
 	// Receive each file info
-	for (int i = 0; i < num; i++) {
+	for (int i = 0; i < num; i++)
+	{
 		int len;
 		recv(clientSock, &len, sizeof(len), 0);
 		server_file_list->file_names[i] = malloc(len);
@@ -221,75 +249,61 @@ file_list_t *receive_file_list(int clientSock) {
 		server_file_list->md5s[i] = malloc(16);
 		recv(clientSock, server_file_list->md5s[i], 16, MSG_WAITALL); // Receive the file md5
 	}
-	
+
 	return server_file_list;
 }
 
 // Create the file list
-void create_file_list(const char *dir_path) {
-	
-	// Helper function to calculate MD5 hash of a file
-	unsigned char *compute_md5(char *file_name) {
-		FILE *file = fopen(file_name, "rb");
-
-		EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-		EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
-
-		unsigned char buffer[1024];
-		int bytes;
-		while ((bytes = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-			EVP_DigestUpdate(mdctx, buffer, bytes);
-		}
-
-		unsigned char *md5_hash = malloc(EVP_MD_size(EVP_md5()));
-		EVP_DigestFinal_ex(mdctx, md5_hash, NULL);
-
-		fclose(file);
-		EVP_MD_CTX_free(mdctx);
-		return md5_hash;
-	}
-	
+void create_file_list(const char *dir_path)
+{
 	DIR *dir = opendir(dir_path);
 	file_list = malloc(sizeof(file_list_t));
-	
+
 	int count = 0;
 	struct dirent *entry;
-	
+
 	// Count files in the directory first
-	while ((entry = readdir(dir)) != NULL) {
-		if (entry->d_type == DT_REG) { // Only regular files
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (entry->d_type == DT_REG)
+		{ // Only regular files
 			count++;
 		}
 	}
-	
+
 	file_list->num_files = count;
 	file_list->file_names = malloc(count * sizeof(char *));
 	file_list->md5s = malloc(count * sizeof(char *));
-	
+
 	// Reset directory stream and fill in file names
 	rewinddir(dir);
 	count = 0;
-	while ((entry = readdir(dir)) != NULL) {
-		if (entry->d_type == DT_REG) { // Only regular files
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (entry->d_type == DT_REG)
+		{														  // Only regular files
 			file_list->file_names[count] = strdup(entry->d_name); // Names
-			
-			char* filepath = malloc(strlen(dir_path) + strlen(entry->d_name) + 2);
+
+			char *filepath = malloc(strlen(dir_path) + strlen(entry->d_name) + 2);
 			strcpy(filepath, dir_path);
 			filepath[strlen(dir_path)] = '/';
 			filepath[strlen(dir_path) + 1] = 0;
 			strcat(filepath, entry->d_name);
 			file_list->md5s[count] = compute_md5(filepath); // md5
-			
+
 			count++;
 		}
 	}
-	
+
 	closedir(dir);
 }
 
-void free_file_list(file_list_t *file_list_ptr) {
-	if (file_list_ptr) {
-		for (int i = 0; i < file_list_ptr->num_files; i++) {
+void free_file_list(file_list_t *file_list_ptr)
+{
+	if (file_list_ptr)
+	{
+		for (int i = 0; i < file_list_ptr->num_files; i++)
+		{
 			free(file_list_ptr->file_names[i]);
 			free(file_list_ptr->md5s[i]);
 		}
@@ -297,4 +311,27 @@ void free_file_list(file_list_t *file_list_ptr) {
 		free(file_list_ptr->md5s);
 		free(file_list_ptr);
 	}
+}
+
+// Helper function to calculate MD5 hash of a file
+unsigned char *compute_md5(char *file_name)
+{
+	FILE *file = fopen(file_name, "rb");
+
+	EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+	EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+
+	unsigned char buffer[1024];
+	int bytes;
+	while ((bytes = fread(buffer, 1, sizeof(buffer), file)) > 0)
+	{
+		EVP_DigestUpdate(mdctx, buffer, bytes);
+	}
+
+	unsigned char *md5_hash = malloc(EVP_MD_size(EVP_md5()));
+	EVP_DigestFinal_ex(mdctx, md5_hash, NULL);
+
+	fclose(file);
+	EVP_MD_CTX_free(mdctx);
+	return md5_hash;
 }
--- server.c
+++ server.c
@@ -1,34 +1,36 @@
 /*Included libraries*/
-#include <stdio.h>			/* For printf() and fprintf() */
-#include <sys/socket.h>		/* For socket(), connect(), send(), and recv() */
-#include <arpa/inet.h>		/* For sockaddr_in and inet_addr() */
-#include <stdlib.h>			/* Supports all sorts of functionality */
-#include <unistd.h>			/* For close() */
-#include <string.h>			/* Support any string ops */
+#include <stdio.h>		/* For printf() and fprintf() */
+#include <sys/socket.h> /* For socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>	/* For sockaddr_in and inet_addr() */
+#include <stdlib.h>		/* Supports all sorts of functionality */
+#include <unistd.h>		/* For close() */
+#include <string.h>		/* Support any string ops */
 
-#include <pthread.h>		/* For thread() */
-#include <dirent.h>			/* Support any file ops */
-#include <openssl/evp.h>	/* Support md5 */
+#include <pthread.h>	 /* For thread() */
+#include <dirent.h>		 /* Support any file ops */
+#include <openssl/evp.h> /* Support md5 */
 
 /* Constants */
 #define PORT 8888
-#define BUFFER_SIZE 1024		/* The send buffer size */
+#define BUFFER_SIZE 1024 /* The send buffer size */
 
 /* Globals */
-typedef struct {
+typedef struct
+{
 	int sock;
-	char client_ip[INET_ADDRSTRLEN];	// For storing client IP
-	int client_port;					// For storing client port
+	char client_ip[INET_ADDRSTRLEN]; // For storing client IP
+	int client_port;				 // For storing client port
 } client_info_t;
 
-typedef struct {
-	int num_files;			// Number of files
-	char **file_names;		// Array of file names
-	unsigned char **md5s;		// Array of md5s
+typedef struct
+{
+	int num_files;		  // Number of files
+	char **file_names;	  // Array of file names
+	unsigned char **md5s; // Array of md5s
 } file_list_t;
 
 pthread_mutex_t lock;
-file_list_t* file_list;
+file_list_t *file_list;
 char PATH[100] = "./music_server";
 
 /* Functions */
@@ -40,54 +42,59 @@ void log_client_activity(client_info_t *client_info, const char *message);
 void send_file_list(int client_sock);
 void create_file_list(const char *dir_path);
 void free_file_list(file_list_t *file_list_ptr);
-
+unsigned char *compute_md5(char *file_name);
 
 /* The main function */
-int main() {
-	int serverSock;						/* Server Socket */
-	int clientSock;						/* Client Socket */
-	struct sockaddr_in serverAddr;		/* Local address */
-	struct sockaddr_in clientAddr;		/* Client address */
-	unsigned int addrLen;				/* Length of address data struct */
-	
+int main()
+{
+	int serverSock;				   /* Server Socket */
+	int clientSock;				   /* Client Socket */
+	struct sockaddr_in serverAddr; /* Local address */
+	struct sockaddr_in clientAddr; /* Client address */
+	unsigned int addrLen;		   /* Length of address data struct */
+
 	// Initialize mutex
 	pthread_mutex_init(&lock, NULL);
 
 	/* Create new TCP Socket for incoming requests*/
-	if ((serverSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+	if ((serverSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
 		printf("socket() failed\n");
 		return 1;
 	}
-	
+
 	serverSock = socket(AF_INET, SOCK_STREAM, 0);
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serverAddr.sin_port = htons(PORT);
 
 	/* Bind to local address structure */
-	if (bind(serverSock, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0){
-		printf("bind() failed\n"); 
+	if (bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+	{
+		printf("bind() failed\n");
 		return 1;
 	}
 
 	/* Listen for incoming connections */
-	if (listen(serverSock, 5) < 0){
+	if (listen(serverSock, 5) < 0)
+	{
 		printf("listen() failed\n");
 		return 1;
 	}
 
 	addrLen = sizeof(struct sockaddr_in);
 	create_file_list(PATH);
-	printf("Server is listening on port %d...\n", PORT); 
+	printf("Server is listening on port %d...\n", PORT);
 
 	// Handle multiple clients using threads
-	while ((clientSock = accept(serverSock, (struct sockaddr*)&clientAddr, &addrLen))) {
+	while ((clientSock = accept(serverSock, (struct sockaddr *)&clientAddr, &addrLen)))
+	{
 		pthread_t client_thread;
 
 		// Allocate memory for client info
 		client_info_t *client_info = malloc(sizeof(client_info_t));
 		client_info->sock = clientSock;
-		
+
 		// Get client IP and port
 		inet_ntop(AF_INET, &(clientAddr.sin_addr), client_info->client_ip, INET_ADDRSTRLEN);
 		client_info->client_port = ntohs(clientAddr.sin_port);
@@ -95,37 +102,47 @@ int main() {
 		// Log client connection
 		log_client_activity(client_info, "Client connected");
 
-		if (pthread_create(&client_thread, NULL, handle_client, (void*)client_info) < 0) {
+		if (pthread_create(&client_thread, NULL, handle_client, (void *)client_info) < 0)
+		{
 			printf("Could not create thread\n");
 			free(client_info);
 			break;
 		}
 	}
-	
+
 	// Cleanup
 	pthread_mutex_destroy(&lock);
 	free_file_list(file_list);
-	close(serverSock); 
-	return 0; 
+	close(serverSock);
+	return 0;
 }
 
 // Handle client communication
-void *handle_client(void *client_info_ptr) {
+void *handle_client(void *client_info_ptr)
+{
 	client_info_t client_info;
 	memcpy(&client_info, client_info_ptr, sizeof(client_info_t));
 	free(client_info_ptr);
 	char client_message[BUFFER_SIZE];
 
-	while (recv(client_info.sock, client_message, sizeof(client_message), 0) > 0) {
+	while (recv(client_info.sock, client_message, sizeof(client_message), 0) > 0)
+	{
 		log_client_activity(&client_info, client_message);
 		// Handle messages from the client
-		if (strncmp(client_message, "LIST", 4) == 0) {
+		if (strncmp(client_message, "LIST", 4) == 0)
+		{
 			list_files(client_info.sock);
-		} else if (strncmp(client_message, "DIFF", 4) == 0) {
+		}
+		else if (strncmp(client_message, "DIFF", 4) == 0)
+		{
 			get_diff(client_info.sock);
-		} else if (strncmp(client_message, "PULL", 4) == 0) {
+		}
+		else if (strncmp(client_message, "PULL", 4) == 0)
+		{
 			send_files(&client_info);
-		} else if (strncmp(client_message, "LEAVE", 5) == 0) {
+		}
+		else if (strncmp(client_message, "LEAVE", 5) == 0)
+		{
 			log_client_activity(&client_info, "Client disconnected");
 			break; // End session
 		}
@@ -136,25 +153,30 @@ void *handle_client(void *client_info_ptr) {
 }
 
 // List files available on the server
-void list_files(int clientSock) {
+void list_files(int clientSock)
+{
 	send_file_list(clientSock);
 }
 
 // Send diff of files between server and client
-void get_diff(int clientSock) {
+void get_diff(int clientSock)
+{
 	send_file_list(clientSock);
 }
 
 // Send files requested by the client
-void send_files(client_info_t *client_info) {
+void send_files(client_info_t *client_info)
+{
 	send_file_list(client_info->sock);
-	
+
 	int index;
-	while (recv(client_info->sock, &index, sizeof(index), 0) > 0) {
-		if(index < 0) {
+	while (recv(client_info->sock, &index, sizeof(index), 0) > 0)
+	{
+		if (index < 0)
+		{
 			break;
 		}
-		
+
 		char buffer[BUFFER_SIZE];
 		FILE *file;
 		size_t bytes_read;
@@ -162,7 +184,7 @@ void send_files(client_info_t *client_info) {
 		// Open the requested file in read mode
 		snprintf(buffer, sizeof(buffer), "%s/%s", PATH, file_list->file_names[index]);
 		file = fopen(buffer, "rb");
-		
+
 		// Get file size
 		fseek(file, 0L, SEEK_END);
 		long file_size = ftell(file);
@@ -170,31 +192,37 @@ void send_files(client_info_t *client_info) {
 		rewind(file);
 
 		// Send the file in chunks
-		while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-			if (send(client_info->sock, buffer, bytes_read, 0) < 0) {
+		while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0)
+		{
+			if (send(client_info->sock, buffer, bytes_read, 0) < 0)
+			{
 				printf("Failed to send file\n");
 				break;
 			}
 		}
 
 		fclose(file);
-		
+
 		snprintf(buffer, sizeof(buffer), "Sent file: %s", file_list->file_names[index]);
 		log_client_activity(client_info, buffer);
 	}
 }
 
 // Log client activity
-void log_client_activity(client_info_t *client_info, const char *message) {
+void log_client_activity(client_info_t *client_info, const char *message)
+{
 	printf("Client (%s:%d): %s\n", client_info->client_ip, client_info->client_port, message);
-	
+
 	pthread_mutex_lock(&lock);
 
 	FILE *log_file = fopen("log.txt", "a");
-	if (log_file != NULL) {
+	if (log_file != NULL)
+	{
 		fprintf(log_file, "Client (%s:%d): %s\n", client_info->client_ip, client_info->client_port, message);
 		fclose(log_file);
-	} else {
+	}
+	else
+	{
 		printf("Could not open log file\n");
 	}
 
@@ -202,84 +230,72 @@ void log_client_activity(client_info_t *client_info, const char *message) {
 }
 
 // Send file list to the client
-void send_file_list(int client_sock) {
+void send_file_list(int client_sock)
+{
 	// First, send the number of files
 	send(client_sock, &file_list->num_files, sizeof(file_list->num_files), 0);
-	
+
 	// Then, send each file name and md5
-	for (int i = 0; i < file_list->num_files; i++) {
+	for (int i = 0; i < file_list->num_files; i++)
+	{
 		int len = strlen(file_list->file_names[i]) + 1;
-		send(client_sock, &len, sizeof(len), 0); // Send the length of the file name
+		send(client_sock, &len, sizeof(len), 0);			 // Send the length of the file name
 		send(client_sock, file_list->file_names[i], len, 0); // Send the file name
-		send(client_sock, file_list->md5s[i], 16, 0); // Send the file md5
+		send(client_sock, file_list->md5s[i], 16, 0);		 // Send the file md5
 	}
 }
 
 // Create the file list
-void create_file_list(const char *dir_path) {
-	
-	// Helper function to calculate MD5 hash of a file
-	unsigned char *compute_md5(char *file_name) {
-		FILE *file = fopen(file_name, "rb");
-
-		EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-		EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
-
-		unsigned char buffer[1024];
-		int bytes;
-		while ((bytes = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-			EVP_DigestUpdate(mdctx, buffer, bytes);
-		}
-
-		unsigned char *md5_hash = malloc(EVP_MD_size(EVP_md5()));
-		EVP_DigestFinal_ex(mdctx, md5_hash, NULL);
-
-		fclose(file);
-		EVP_MD_CTX_free(mdctx);
-		return md5_hash;
-	}
-	
+void create_file_list(const char *dir_path)
+{
 	DIR *dir = opendir(dir_path);
 	file_list = malloc(sizeof(file_list_t));
-	
+
 	int count = 0;
 	struct dirent *entry;
-	
+
 	// Count files in the directory first
-	while ((entry = readdir(dir)) != NULL) {
-		if (entry->d_type == DT_REG) { // Only regular files
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (entry->d_type == DT_REG)
+		{ // Only regular files
 			count++;
 		}
 	}
-	
+
 	file_list->num_files = count;
 	file_list->file_names = malloc(count * sizeof(char *));
 	file_list->md5s = malloc(count * sizeof(unsigned char *));
-	
+
 	// Reset directory stream and fill in file names
 	rewinddir(dir);
 	count = 0;
-	while ((entry = readdir(dir)) != NULL) {
-		if (entry->d_type == DT_REG) { // Only regular files
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (entry->d_type == DT_REG)
+		{														  // Only regular files
 			file_list->file_names[count] = strdup(entry->d_name); // Names
-			
-			char* filepath = malloc(strlen(dir_path) + strlen(entry->d_name) + 2);
+
+			char *filepath = malloc(strlen(dir_path) + strlen(entry->d_name) + 2);
 			strcpy(filepath, dir_path);
 			filepath[strlen(dir_path)] = '/';
 			filepath[strlen(dir_path) + 1] = 0;
 			strcat(filepath, entry->d_name);
 			file_list->md5s[count] = compute_md5(filepath); // md5
-			
+
 			count++;
 		}
 	}
-	
+
 	closedir(dir);
 }
 
-void free_file_list(file_list_t *file_list_ptr) {
-	if (file_list_ptr) {
-		for (int i = 0; i < file_list_ptr->num_files; i++) {
+void free_file_list(file_list_t *file_list_ptr)
+{
+	if (file_list_ptr)
+	{
+		for (int i = 0; i < file_list_ptr->num_files; i++)
+		{
 			free(file_list_ptr->file_names[i]);
 			free(file_list_ptr->md5s[i]);
 		}
@@ -287,4 +303,27 @@ void free_file_list(file_list_t *file_list_ptr) {
 		free(file_list_ptr->md5s);
 		free(file_list_ptr);
 	}
+}
+
+// Helper function to calculate MD5 hash of a file
+unsigned char *compute_md5(char *file_name)
+{
+	FILE *file = fopen(file_name, "rb");
+
+	EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+	EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+
+	unsigned char buffer[1024];
+	int bytes;
+	while ((bytes = fread(buffer, 1, sizeof(buffer), file)) > 0)
+	{
+		EVP_DigestUpdate(mdctx, buffer, bytes);
+	}
+
+	unsigned char *md5_hash = malloc(EVP_MD_size(EVP_md5()));
+	EVP_DigestFinal_ex(mdctx, md5_hash, NULL);
+
+	fclose(file);
+	EVP_MD_CTX_free(mdctx);
+	return md5_hash;
 }
